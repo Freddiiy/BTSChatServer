2cd49ba7c5dc13e412e7d68a0e9ea157bcbfaba9
Freddiiy <frederikgaller@live.dk>
2021-09-28 00:21:35 +0200

    bad changes

--- src/main/java/compliant/ClientHandler.java
+++ src/main/java/compliant/ClientHandler.java
@@ -3,11 +3,12 @@ package compliant;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
     private Socket client;
     private PrintWriter pw;
     private Scanner scanner;
@@ -47,43 +48,59 @@ public class ClientHandler implements Runnable{
 
     public void protocol() {
         String msg = "";
-        pw.println("What is your name?: ");
-        try {
-            clientName = scanner.nextLine();
-        } catch (NoSuchElementException e) {
-            System.out.println("unexpected loss of connection to client");
-        }
         onlineList.add(this);
 
-        pw.println("Hello " + this.clientName + " and welcome. \n---------------------------------------\n" +
-                "Online users: " + onlineList.getOnlineUsers()+ "\n---------------------------------------\n");
-
-        while (!msg.toUpperCase().equals("/CLOSE")) {
+        while (!msg.toUpperCase().equals("CLOSE#")) {
             try {
                 msg = scanner.nextLine();
             } catch (NoSuchElementException e) {
-                System.out.println("unexpected loss of connection to client");
+                e.printStackTrace();
+                System.out.println("Unexpected loss of connection to client");
                 break;
             }
+
+            String[] nameSplit = msg.split("#", 2);
+            if (!nameSplit[0].equals("CONNECT#")) {
+                try {
+                    pw.println("Didnt write CONNECT#'name'. Try to reconnect");
+                    client.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+
+            clientName = nameSplit[1];
+            System.out.println("Client name: " + clientName);
+            msg = "";
 
             String command;
             String data;
             try {
-                String[] action = msg.split(" ", 2);
+                String[] action = msg.split("#", 2);
+//                System.out.println(Arrays.toString(action));
                 command = action[0].toUpperCase();
                 data = action[1];
 
                 switch (command) {
-                    case "/ALL" ->
-                            //INSERT MESSAGE IN SHARED RESOURCE
-                            sendToAll(clientName, "ALL", data);
-                    case "/MSG" -> {
-                        String dataArray[] = data.split(" ", 2);
+                    case "SEND" -> {
+                        String[] dataArray = data.split("#", 2);
+
+                        System.out.println(Arrays.toString(dataArray));
+
                         String msgTo = dataArray[0].toUpperCase();
                         data = dataArray[1];
                         sendMsgTo(clientName, msgTo, data);
                     }
-                    case "/ONLINE" -> pw.println("Online users: " + onlineList.getOnlineUsers());
+                    case "ONLINE" -> pw.println("Online users: " + onlineList.getOnlineUsers());
+                    case "CLOSE" -> {
+                        try {
+                            pw.println("Closing connection...");
+                            client.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                     default -> pw.println("Not recognised");
                 }
             } catch (ArrayIndexOutOfBoundsException e) {
@@ -113,13 +130,8 @@ public class ClientHandler implements Runnable{
         return null;
     }
 
-    public void sendToAll(String clientName, String msgTo, String input) {
-        System.out.println(clientName + " to ALL " + input);
-        queue.add(new MessageHandler(clientName, msgTo, input));
-    }
-
-    public void sendMsgTo(String clientName,String msgTo, String input) {
-        System.out.println(clientName + " to " + msgTo + ": " + input);
+    public void sendMsgTo(String clientName, String msgTo, String input) {
+        System.out.println(clientName + "#" + msgTo + "#" + input);
         queue.add(new MessageHandler(clientName, msgTo, input));
     }
 
