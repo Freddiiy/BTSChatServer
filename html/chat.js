07e702001604365181e73cc76b99a246b1fcdfa7
Christian C <cph-cs450@cphbusiness.dk>
2021-09-30 10:56:11 +0200

    nyt

--- html/chat.js
+++ html/chat.js
@@ -5,18 +5,18 @@ const messageInput = document.getElementById('message-input')
 
 const username = prompt
 appendenMessage('you have joined')
-websocket.emit('new-user', username)
+socket.emit('new-user', username)
 
-websocket.on('chat-massages',data =>{
+bsocket.on('chat-massages',data =>{
     appendenMessage(data.username, data.message)
 })
 
 
-websocket.on('user-connected', username =>{
+bsocket.on('user-connected', username =>{
     appendenMessage(username, 'connected')
 })
 
-websocket.on('user-disconnected', username =>{
+bsocket.on('user-disconnected', username =>{
     appendenMessage(username, 'disconnected')
 })
 
@@ -25,7 +25,7 @@ messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message= messageInput.value
     appendenMessage('You: ${message}')
-    websocket.emit('send-chat-message',message)
+    bsocket.emit('send-chat-message',message)
     messageInput.value = ''
 }) 
     function appendenMessage(message){
